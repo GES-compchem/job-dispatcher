e462cb124fac5368b0f27d894525d39ac15f7661
GES-mpalermo <mattia.palermo@greenenergystorage.eu>
2022-04-30 13:29:16 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name="job-dispatcher",
-    version="0.1.6",
+    version="0.1.7",
     description="",
     long_description="",
     packages=["jobdispatcher"],
